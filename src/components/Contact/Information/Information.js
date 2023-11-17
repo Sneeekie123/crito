5c787c2a897d62e66f05da6822f2687f5de2a940
Sneeekie123 <143397337+Sneeekie123@users.noreply.github.com>
2023-11-17 10:23:11 +0100

    Färdig om jag ej hittar mitt error!!!!

--- src/components/Contact/Information/Information.js
+++ src/components/Contact/Information/Information.js
@@ -3,21 +3,43 @@ import './Information.css';
 
 const Information = () => {
   const [errors, setErrors] = useState({});
-  const [responseData, setResponseData] = useState(null);
+  const [responseData, setResponseData] = useState();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   const register = (event) => {
     event.preventDefault();
     setErrors({});
 
-    for (let element of event.target.elements) {
-      if (element.tagName.toLowerCase() === 'input' && element.required) {
-        validateFormField(element);
-      }
+    const err = {
+      name: '',
+      email: '',
+      message: '',
+    };
+
+    if (name.length < 2) {
+      err['name'] = 'You must enter a valid name';
+      setResponseData(null);
     }
 
-    if (Object.values(errors).every((error) => error === '')) {
-      const formData = new FormData(event.target);
-      const json = JSON.stringify(Object.fromEntries(formData));
+    if (!emailRegex.test(email)) {
+      err['email'] = 'Please enter a valid email address';
+      setResponseData(null); 
+    }
+
+    if (message.length < 5) {
+      err['message'] = 'Your message is too short';
+      setResponseData(null);
+    }
+
+    setErrors(err);
+
+    if (Object.values(err).every((error) => error === '')) {
+      const formData = {name, email, message};
+      const json = JSON.stringify(formData);
 
       fetch('https://win23-assignment.azurewebsites.net/api/contactform', {
         method: 'post',
@@ -27,7 +49,7 @@ const Information = () => {
         body: json,
       })
         .then((res) => {
-          if (res.status > 201) {
+          if (res.status !== 200) {
             throw new Error('Bad Request');
           }
           return res.text();
@@ -35,49 +57,13 @@ const Information = () => {
         .then((data) => {
           console.log(data);
           setResponseData(data);
+          setName('');
+          setEmail('');
+          setMessage('');
         })
         .catch((error) => {
-          setResponseData('');
+          console.error('Error:', error);
         });
-    }
-  };
-
-  const validateFormField = (targetElement) => {
-    const value = targetElement.value.trim();
-    const type = targetElement.type;
-    const id = targetElement.id;
-
-    switch (type) {
-      case 'text':
-      case 'textarea':
-        validateLength(id, value, 2);
-        break;
-
-      case 'email':
-        validateEmail(id, value);
-        break;
-
-      default:
-        break;
-    }
-  };
-
-
-
-  const validateLength = (id, value, minLength) => {
-    if (value.length < minLength) {
-      setErrors((prevErrors) => ({ ...prevErrors, [id]: `You must enter a valid ${id}` }));
-    } else {
-      setErrors((prevErrors) => ({ ...prevErrors, [id]: '' }));
-    }
-  };
-
-  const validateEmail = (id, value) => {
-    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (!emailRegex.test(value)) {
-      setErrors((prevErrors) => ({ ...prevErrors, [id]: `Please enter a valid ${id}` }));
-    } else {
-      setErrors((prevErrors) => ({ ...prevErrors, [id]: '' }));
     }
   };
 
@@ -90,16 +76,16 @@ const Information = () => {
         <div className="message-boxes">
           <form action="testing.php" onSubmit={register} method="post" noValidate>
             <label htmlFor="name"></label>
-            <input type="text" name="name" id="name" title="name" placeholder="Name*" required />
+            <input type="text" name="name" id="name" title="name" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name*" required />
             <span className="span" id="name-error">{errors['name']}</span>
 
             <label htmlFor="email"></label>
-            <input type="email" id="email" name="email" placeholder="username@domain.com" required />
-            <span className="span" id="email-error">{errors['email']}</span>
+            <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="username@domain.com"required />
+            <span className="span" id="email-error"> {errors['email']} </span>
 
             <label htmlFor="message"></label>
-            <input className="message" name="message" id="message" title="message" placeholder="Your message*" required />
-            <span className="span" id="message-error">{errors['message']}</span>
+            <input className="message" name="message" id="message" title="message" value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Your message*" required />
+            <span className="span" id="message-error"> {errors['message']} </span>
             <span className="logdata">{responseData}</span>
 
             <button type="submit" className="btn-yellow btn-login">
