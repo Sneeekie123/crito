754377d9d1e5c41e850425350aeb52ad8b24d186
Sneeekie123 <143397337+Sneeekie123@users.noreply.github.com>
2023-11-17 11:13:30 +0100

    Färdig om jag ej hittar mitt error!!!!

--- src/components/Main/Newsletter/Newsletter.js
+++ src/components/Main/Newsletter/Newsletter.js
@@ -1,82 +1,45 @@
-import React from 'react';
-import background_wavy_lines from '../../../assets/images/background-wavy-lines.svg'; 
-import './Newsletter.css'
-
-
+import React, { useState } from 'react';
+import background_wavy_lines from '../../../assets/images/background-wavy-lines.svg';
+import './Newsletter.css';
 
 const Newsletter = () => {
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
 
-  const register = (event) => {
-    event.preventDefault();
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    for (let element of event.target.elements) {
-      if (element.required) {
-        validateFormField(element);
-      }
-    }
-  }; 
-
-  const validateFormField = (targetElement) => {
-    const errorMessages = {
-      email_required: 'You must enter a Email adress',
-      email_invalid: 'Please enter a valid Email address',
-    };
-
-    if (!validateLength(targetElement.value, 1 )) {
-      document.getElementById(`${targetElement.id}`).classList.add('error');
-      document.getElementById(`${targetElement.id}-error`).innerHTML = errorMessages[targetElement.id + '_required'];
-    }
-    else {
-
-      var result = false
-      switch(targetElement.type) {
-          case 'text' :
-              result = validateLength(targetElement)
-              break;
-
-          case 'email':
-              result = validateEmail(targetElement.value)
-              break;
-         
-      }
-      if (!result) {
-          document.getElementById(`${targetElement.id}`).classList.add('error')
-          document.getElementById(`${targetElement.id}-error`).innerHTML = errorMessages[targetElement.id + "_invalid"]
-      } else  {
-          document.getElementById(`${targetElement.id}`).classList.remove('error')
-          document.getElementById(`${targetElement.id}-error`).innerHTML = ""
-      }  
-    } 
-  }; 
   
-  function validateLength(value, minLength = 2) {
-    if (value.length >= minLength)
-        return true 
+  const register = (e) => {
+    e.preventDefault();
+    validateEmail();
 
-    return false
-}
-
-  const validateEmail = (value) => {
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value))
-        return true
-
-    return false
+    
   };
+
+  const validateEmail = () => {
+    if (!emailRegex.test(email)) {
+      setEmailError('Please enter a valid email address');
+    } else {
+      setEmailError('Email valid')
+      console.log('Email valid');
+    }
+  };
+
+
 
   return (
     <section className="newsletter">
       <img className="background-wavy-lines" src={background_wavy_lines} alt="" />
       <div className="container">
         <h2>Get News Updates By Signup</h2>
-        <form id='regForm' className='continer'  onSubmit={register} method="post" noValidate>
+        <form id="regForm" onSubmit={register} method="post" noValidate>
           <div>
-            <input id="email" required type="email" placeholder="username@domain.com" />
-            <span className='container span' id="email-error"></span>
+            <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} onBlur={validateEmail} placeholder="username@domain.com" required/>
+            <span className="container span" id="email-error">{emailError}</span>
           </div>
           <div>
-          <button className="btn-yellow" type="submit"> Subscribe </button>
+            <button className="btn-yellow" type="submit">Subscribe</button>
           </div>
-           
         </form>
       </div>
     </section>
