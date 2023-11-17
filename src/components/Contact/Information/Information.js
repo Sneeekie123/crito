54d69a9e928b45c850030479ca84c9b9af2f1484
Sneeekie123 <143397337+Sneeekie123@users.noreply.github.com>
2023-11-17 01:30:39 +0100

    responsedata fixat

--- src/components/Contact/Information/Information.js
+++ src/components/Contact/Information/Information.js
@@ -3,10 +3,11 @@ import './Information.css';
 
 const Information = () => {
   const [errors, setErrors] = useState({});
+  const [responseData, setResponseData] = useState(null);
 
   const register = (event) => {
     event.preventDefault();
-    setErrors({}); // Reset errors
+    setErrors({});
 
     for (let element of event.target.elements) {
       if (element.tagName.toLowerCase() === 'input' && element.required) {
@@ -14,20 +15,31 @@ const Information = () => {
       }
     }
 
-    if (Object.values(errors).every(error => error === '')) {
-      // No errors, proceed to submit
+    if (Object.values(errors).every((error) => error === '')) {
       const formData = new FormData(event.target);
       const json = JSON.stringify(Object.fromEntries(formData));
 
-      fetch("https://win23-assignment.azurewebsites.net/api/contactform", {
-        method: "post",
+      fetch('https://win23-assignment.azurewebsites.net/api/contactform', {
+        method: 'post',
         headers: {
-          "Content-Type": "application/json"
+          'Content-Type': 'application/json',
         },
-        body: json
+        body: json,
       })
-        .then(res => res.text())
-        .then(data => console.log(data));
+        .then((res) => {
+          if (res.status > 201) {
+            throw new Error('Bad Request');
+          }
+          return res.text();
+        })
+        .then((data) => {
+          console.log(data);
+          setResponseData(data);
+        })
+        .catch((error) => {
+          
+          setResponseData(null);
+        });
     }
   };
 
@@ -36,7 +48,6 @@ const Information = () => {
     const type = targetElement.type;
     const id = targetElement.id;
 
-    // Validation logic based on input type
     switch (type) {
       case 'text':
       case 'textarea':
@@ -54,18 +65,18 @@ const Information = () => {
 
   const validateLength = (id, value, minLength) => {
     if (value.length < minLength) {
-      setErrors(prevErrors => ({ ...prevErrors, [id]: `You must enter a valid ${id}` }));
+      setErrors((prevErrors) => ({ ...prevErrors, [id]: `You must enter a valid ${id}` }));
     } else {
-      setErrors(prevErrors => ({ ...prevErrors, [id]: '' }));
+      setErrors((prevErrors) => ({ ...prevErrors, [id]: '' }));
     }
   };
 
   const validateEmail = (id, value) => {
     const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (!emailRegex.test(value)) {
-      setErrors(prevErrors => ({ ...prevErrors, [id]: `Please enter a valid ${id}` }));
+      setErrors((prevErrors) => ({ ...prevErrors, [id]: `Please enter a valid ${id}` }));
     } else {
-      setErrors(prevErrors => ({ ...prevErrors, [id]: '' }));
+      setErrors((prevErrors) => ({ ...prevErrors, [id]: '' }));
     }
   };
 
@@ -76,19 +87,20 @@ const Information = () => {
           <h2>Leave us a message for any information.</h2>
         </div>
         <div className="message-boxes">
-          <form action='testing.php' onSubmit={register} method="post" noValidate>
+          <form action="testing.php" onSubmit={register} method="post" noValidate>
             <label htmlFor="name"></label>
             <input type="text" name="name" id="name" title="name" placeholder="Name*" required />
-            <span className='span' id="name-error">{errors['name']}</span>
-            
+            <span className="span" id="name-error">{errors['name']}</span>
+
             <label htmlFor="email"></label>
             <input type="email" id="email" name="email" placeholder="username@domain.com" required />
-            <span className='span' id="email-error">{errors['email']}</span>
-            
+            <span className="span" id="email-error">{errors['email']}</span>
+
             <label htmlFor="message"></label>
             <input className="message" name="message" id="message" title="message" placeholder="Your message*" required />
-            <span className='span' id="message-error">{errors['message']}</span>
-            
+            <span className="span" id="message-error">{errors['message']}</span>
+            <span className="logdata">{responseData}</span>
+
             <button type="submit" className="btn-yellow btn-login">
               Send Message<i className="fa-regular fa-arrow-up-right"></i>
             </button>
